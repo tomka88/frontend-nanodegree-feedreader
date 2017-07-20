5fbeb6691114b747871fb80aff12595660840a92
Tamas Kocsis <kocsis.tamas88@gmail.com>
2017-07-20 09:51:19 +0200

    added readme, cleaned up specs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -40,7 +40,7 @@ $(function() {
             
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -54,11 +54,9 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
     describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -69,7 +67,7 @@ $(function() {
             expect(body.classList).toContain('menu-hidden');
         });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -83,10 +81,10 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    
     describe('Initial Entries', function() {
 
-        /* TODO: Write a test that ensures when the loadFeed
+        /* when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -102,11 +100,10 @@ $(function() {
     
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
 
     describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
